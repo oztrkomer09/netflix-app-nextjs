766470be16756a5bbbf15f643c5124b9bff3f52a
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-03-28 23:37:22 +0300

    add: create movie detail page for every single movie

---
+++ app/movie/[id]/page.js
@@ -0,0 +1,16 @@
+import MovieContainer from "@/containers/movie";
+import Movies from "@/mocks/movies.json";
+import { notFound } from "next/navigation";
+
+function MoviePage({ params }) {
+  const movieDetail = Movies.results.find(
+    (movie) => movie.id.toString() === params.id
+  );
+  if (!movieDetail) {
+    notFound();
+  }
+
+  return <MovieContainer movie={movieDetail} />;
+}
+
+export default MoviePage;
---
+++ containers/movie/index.js
@@ -0,0 +1,11 @@
+import { FeaturedMovie } from "@/components/featured-movie";
+
+function MovieContainer({ movie }) {
+  return (
+    <div>
+      <FeaturedMovie movie={movie} isCompact={false} />
+    </div>
+  );
+}
+
+export default MovieContainer;
