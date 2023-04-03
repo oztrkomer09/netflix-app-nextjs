fff3941aa97e9ad3cfb04404c11c490f6677a385
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-04-03 22:21:02 +0300

    add: implement loaders

--- app/[[...category]]/loading.js
+++ app/[[...category]]/loading.js
@@ -1,7 +1,17 @@
 import CategoriesLoading from "@/components/categories-section/loading";
+import FeatureMovieLoading from "@/components/featured-movie/loading";
+import MoviesSectionLoading from "@/components/movies-section/loading";
 
 function Loading() {
-  return <CategoriesLoading />;
+  return (
+    <div>
+      <FeatureMovieLoading />
+      <CategoriesLoading />
+      <MoviesSectionLoading />
+      <MoviesSectionLoading />
+      <MoviesSectionLoading />
+    </div>
+  );
 }
 
 export default Loading;
